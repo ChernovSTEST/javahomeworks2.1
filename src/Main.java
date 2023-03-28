38cc9b3e37d683fa3e2955b1a8ab863e1d05a4d1
Test <test@test.com>
2023-03-28 13:47:41 +0300

    mile

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 18_000;
+        int bonus = 20;
+        int mile = cost / bonus;
+        System.out.println(mile);
+    }
+}
